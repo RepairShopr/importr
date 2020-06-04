c2f981b0872e5a436c0b0fa9bd99958296c90159
Stanislav Hriaznov <st.gryaznov@gmail.com>
2020-06-04 14:16:19 +0300

    5.0 - 5.1

--- config/application.rb
+++ config/application.rb
@@ -20,10 +20,5 @@ module Importr
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
-    ActiveSupport.halt_callback_chains_on_return_false = false
   end
 end
