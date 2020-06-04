ba97a235dad8928faeaa0145effb4c42f3683b21
Stanislav Hriaznov <st.gryaznov@gmail.com>
2020-06-04 11:01:35 +0300

    Fix existing tests.

--- test/controllers/imports_controller_test.rb
+++ test/controllers/imports_controller_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class ImportsControllerTest < ActionController::TestCase
@@ -8,19 +10,15 @@ class ImportsControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:imports)
   end
 
   test "should get new" do
     get :new
-    assert_response :success
+    assert_redirected_to import_path(assigns(:import))
   end
 
   test "should create import" do
-    assert_difference('Import.count') do
-      post :create, import: { api_key: @import.api_key, error_count: @import.error_count, mapping: @import.mapping, record_count: @import.record_count, resource_type: @import.resource_type, success_count: @import.success_count }
-    end
-
+    assert_difference('Import.count') { post :create, import: import_params }
     assert_redirected_to import_path(assigns(:import))
   end
 
@@ -34,16 +32,24 @@ class ImportsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should destroy import" do
+    assert_difference('Import.count', -1) { delete :destroy, id: @import }
+    assert_redirected_to imports_path
+  end
+
   test "should update import" do
-    patch :update, id: @import, import: { api_key: @import.api_key, error_count: @import.error_count, mapping: @import.mapping, record_count: @import.record_count, resource_type: @import.resource_type, success_count: @import.success_count }
+    patch :update, id: @import, import: import_params
     assert_redirected_to import_path(assigns(:import))
   end
 
-  test "should destroy import" do
-    assert_difference('Import.count', -1) do
-      delete :destroy, id: @import
-    end
-
-    assert_redirected_to imports_path
+  def import_params
+    {
+      api_key:       @import.api_key,
+      error_count:   @import.error_count,
+      mapping:       @import.mapping,
+      record_count:  @import.record_count,
+      resource_type: @import.resource_type,
+      success_count: @import.success_count
+    }
   end
 end
